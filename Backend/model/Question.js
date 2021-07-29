9142c99a763aabcb68632aba75684133b4951dca
Anubhav Jain <anubhavjain.jain0@gmail.com>
2021-07-29 15:35:39 +0530

    Update API

--- Backend/model/Question.js
+++ Backend/model/Question.js
@@ -1,8 +1,12 @@
 const mongoose = require("mongoose");
 
 const question_schema = mongoose.Schema({
-  id: Number,
-  question: String
+  question: String,
+  correct_answer:String,
+  incorrect_answer1:String,
+  incorrect_answer2:String,
+  incorrect_answer3:String,
+
 })
 
 module.exports = mongoose.model("Question",question_schema)
--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,7 @@ const express = require("express")
 const app = express();
 const mongoose = require("mongoose")
 const bodyParser = require("body-parser")
+const morgan = require("morgan")
 
 //database Connection
 require("./mongo")
@@ -12,6 +13,7 @@ require("./model/Question")
 
 //MiddleWare
 app.use(bodyParser.json())
+   .use(morgan())
 
 const Question = mongoose.model("Question")
 
@@ -27,8 +29,13 @@ app.get("/OnlineExaminationSystem",async function(req,res){
 app.post("/OnlineExaminationSystem",async function(req,res){
      try {
       const question = new Question();
-      question.id = req.body.id;
-      question.question = req.body.question;
+
+    question.question = req.body.question;
+    question.correct_answer = req.body.correct_answer;
+    question.incorrect_answer1 = req.body.incorrect_answer1;
+    question.incorrect_answer2 = req.body.incorrect_answer2;
+    question.incorrect_answer3 = req.body.incorrect_answer3;
+
 
       await question.save()
       res.send(question)
@@ -37,6 +44,40 @@ app.post("/OnlineExaminationSystem",async function(req,res){
      res.status(500)
   }
 });
+
+app.get("/OnlineExaminationSystem/:QId",async function(req,res){
+  try {
+    const question = await Question.findOne({ _id:req.params.QId})
+    res.send(question)
+  } catch (error) {
+    res.status(500)
+  }
+});
+
+app.put("/OnlineExaminationSystem/:QId",async function(req,res){
+  try {
+    const question = await Question.findByIdAndUpdate({
+      _id:req.params.QId
+    },req.body,{
+      new:true
+    //  runValidators:true
+    });
+    res.send(question)
+  } catch (error) {
+    res.status(500)
+  }
+});
+
+app.delete("/OnlineExaminationSystem/:QId",async function(req,res){
+  try {
+    const question = await Question.findByIdAndRemove({
+      _id:req.params.QId
+    });
+    res.send(question)
+  } catch (error) {
+    res.status(500)
+  }
+  });
 
 
 app.listen(777, function () {
